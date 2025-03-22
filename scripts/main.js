36b5afefd57d7051b4a58fb4010b249373e4ae3f
LibraAkaja <adhikaribiraj908@gmail.com>
2025-03-22 13:39:40 +0545

    Add script for index page

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+import { navAnimation, mainPageAnimation} from "./animation.js";
+import { removeDElement } from "./dynamic.js";
+
+window.addEventListener("load",()=>{
+    navAnimation();
+    mainPageAnimation();
+});
+
+document.querySelector("#btn1").addEventListener("click",()=>{
+    window.location.href = "../pages/generator.html"; 
+},{once:true});
+
+document.querySelector("#btn2").addEventListener("click",()=>{
+    window.location.href = "../pages/reader.html"; 
+},{once:true});
