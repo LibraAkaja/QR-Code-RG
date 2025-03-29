34f6b2e4733e43d87558220351d5ea5440e9b25a
LibraAkaja <adhikaribiraj908@gmail.com>
2025-03-29 16:13:31 +0545

    Add script for QR code generator page

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,6 +1,46 @@
 import { navAnimation, otherPageAnimation } from "./animation.js";
+import { addText, changeCSS, createDElement, removeDElement, removeElements } from "./dynamic.js";
 
 window.addEventListener("load",()=>{
     navAnimation();
     otherPageAnimation("#bx1");
 });
+
+document.querySelector(".QRCGbtn").addEventListener("click",()=>{
+    const bx2Clone = document.querySelector("#bx2").cloneNode(true); // Needed if the user tries to go one step back
+    const qrText = document.querySelector(".text4QR").value;    // Getting the text or URL entered by User
+    if(qrText.trim()===""){
+        alert("Please enter Text or URL first!");
+        return;
+    }
+    else{
+        removeElements(["p",".text4QR",".QRCGbtn"]);
+        createDElement("#bx2","div","loaderContainer");
+        createDElement(".loaderContainer","div","loader");
+        createDElement(".loader","div","box1");
+        createDElement(".loader","div","box2");
+        createDElement(".loader","div","box3");
+        const loaderClone = document.querySelector(".loaderContainer").cloneNode(true); // Needed if user tries generation multiple times
+        createDElement("#bx2","div","qrContainer");
+        changeCSS(".qrContainer","display","none");
+        setTimeout(()=>{
+            generateQR(qrText);
+        },2500);
+        setTimeout(()=>{
+            removeDElement(".loaderContainer");
+            changeCSS(".qrContainer","display","block");
+            createDElement(".qrContainer","div","downloadContainer");
+            createDElement(".downloadContainer","div","dIcon");
+        },10000);
+    }
+});
+
+function generateQR(qrText){
+    const qrContainer = document.querySelector(".qrContainer");
+    qrContainer.innerHTML = "";
+    new QRCode(qrContainer,{
+        text: qrText,
+        width: 200,
+        height: 200,
+    });    
+}
